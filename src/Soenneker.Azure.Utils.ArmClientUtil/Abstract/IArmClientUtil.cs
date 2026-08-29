742eb2664e71e1f495bd24f4a521e16a3d4dde2a
soenneker <jake@soenneker.com>
2026-08-29 14:16:47 -0500

    Improve public API XML documentation

--- src/Soenneker.Azure.Utils.ArmClientUtil/Abstract/IArmClientUtil.cs
+++ src/Soenneker.Azure.Utils.ArmClientUtil/Abstract/IArmClientUtil.cs
@@ -11,9 +11,9 @@ namespace Soenneker.Azure.Utils.ArmClientUtil.Abstract;
 public interface IArmClientUtil : IAsyncDisposable, IDisposable
 {
     /// <summary>
-    /// Gets the value.
+    /// Returns the configured arm Client used by the arm client.
     /// </summary>
-    /// <param name="cancellationToken">The cancellation token.</param>
-    /// <returns>A task containing the result of the operation.</returns>
+    /// <param name="cancellationToken">Token used to cancel the operation.</param>
+    /// <returns>A task whose result is the requested arm Client.</returns>
     ValueTask<ArmClient> Get(CancellationToken cancellationToken = default);
 }
--- src/Soenneker.Azure.Utils.ArmClientUtil/Registrars/ArmClientUtilRegistrar.cs
+++ src/Soenneker.Azure.Utils.ArmClientUtil/Registrars/ArmClientUtilRegistrar.cs
@@ -12,6 +12,8 @@ public static class ArmClientUtilRegistrar
     /// <summary>
     /// Adds <see cref="IArmClientUtil"/> as a singleton service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddArmClientUtilAsSingleton(this IServiceCollection services)
     {
         services.TryAddSingleton<IArmClientUtil, ArmClientUtil>();
@@ -22,6 +24,8 @@ public static class ArmClientUtilRegistrar
     /// <summary>
     /// Adds <see cref="IArmClientUtil"/> as a scoped service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddArmClientUtilAsScoped(this IServiceCollection services)
     {
         services.TryAddScoped<IArmClientUtil, ArmClientUtil>();
